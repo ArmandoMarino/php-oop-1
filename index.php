70191409c999b38101ea52b39ee743f4ff768a98
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-02-17 17:10:00 +0100

    update variables

--- index.php
+++ index.php
@@ -2,15 +2,29 @@
 
 class Movie
 {
+    // Variabili d'istanza (perchè variano a secondo dell'istanza)
     public $title;
     public $year;
     public $lang;
     public $genre;
     public $poster;
+
+    public function __construct($_title, $_year, $_lang, $_genre, $_poster)
+    {
+        $this->title = $_title;
+        $this->year = $_year;
+        $this->lang = $_lang;
+        $this->genre = $_genre;
+        $this->poster = $_poster;
+    }
 }
 
 
 $dayafter = new Movie();
+
+
+
+
 
 
 
