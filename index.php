b3c30a1add407572b63dcd906b383e83c2ebf227
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-02-17 17:19:31 +0100

    first movie update

--- index.php
+++ index.php
@@ -3,24 +3,25 @@
 class Movie
 {
     // Variabili d'istanza (perchè variano a secondo dell'istanza)
+    public $id;
     public $title;
     public $year;
     public $lang;
-    public $genre;
     public $poster;
 
-    public function __construct($_title, $_year, $_lang, $_genre, $_poster)
+    //costruttore che prepara le variabili che in questo caso i movies dovranno avere
+    public function __construct($_id, $_title, $_year, $_lang, $_poster)
     {
+        $this->id = $_id;
         $this->title = $_title;
         $this->year = $_year;
         $this->lang = $_lang;
-        $this->genre = $_genre;
         $this->poster = $_poster;
     }
 }
 
 
-$dayafter = new Movie();
+$dayafter = new Movie(1, 'Cuando Sea Joven','2022-09-14', 'es', '/6gIJuFHh5Lj4dNaPG3TzIMl7L68.jpg');
 
 
 
